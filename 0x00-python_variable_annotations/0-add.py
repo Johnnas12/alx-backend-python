6826a92e3bb99dc5d2995f37e3677aae89cb1af6
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-07-05 22:24:00 +0300

    done

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,9 @@
-def add (a: float, b: float) -> float:
+#!/usr/bin/env python3
+'''Modules of task-0.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers.
+    '''
     return a + b
